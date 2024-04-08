56ac5a98f6e7169bfd4188a505c0ebb9e05ea3bc
Gennadiy <renardes@gmail.com>
2024-04-08 19:16:06 +0500

    встроенная функция print(): именованный параметр sep

---
+++ base/print3.py
@@ -0,0 +1,8 @@
+print('Aa', 'Bb', 'Cc', 'Dd', sep='.')
+
+print('line 1', 'line 2', 'line 3', sep='\n', end='\n\n')
+
+print(1, 2, 3, 4, 5, sep=' # ', end='\n\n')
+
+print('один', sep='_разделитель_', end='_конец_строки')
+print(sep='_разделитель_', end='_конец_строки')
