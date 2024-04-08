8f9dd53d3a78579aa5b669e1eacfd9e9c4f9bc11
Gennadiy <renardes@gmail.com>
2024-04-08 19:19:29 +0500

    литералы языка Python

---
+++ base/literals.py
@@ -0,0 +1,50 @@
+# int
+33
+
+# float
+4.09
+.89
+
+# complex
+1-3j
+1j
+
+# bool
+True
+False
+
+
+# str
+'он сказал: "сегодня дождливо"'
+
+"it's rainy"
+
+'he said: \"it\'s rainy\"'
+
+'''he
+said:
+ "it's rainy"'''
+
+"""строчка 1
+строчка 2
+строчка 3"""
+
+'C:\\Program Files\\Notepad++\\notepad++.exe'
+
+r'C:\Program Files\Notepad++\notepad++.exe'
+
+b'0x700x720xA1'
+
+
+# tuple
+(10, )
+(1, 3.21, 'word')
+
+# list
+['text', 7, .95]
+
+# dict
+{'key 1': 190, 46: [.1, .2]}
+
+# set
+{'a', 1, .1, (4, 5)}
