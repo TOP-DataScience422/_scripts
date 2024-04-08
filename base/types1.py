33b5d5fcd1e41879d82a980762eca7017675d646
Gennadiy <renardes@gmail.com>
2024-04-08 19:19:56 +0500

    преобразование объектов разных типов в тип int

---
+++ base/types1.py
@@ -0,0 +1,15 @@
+print('int(2001) =', int(2001))
+
+print('int(3.89) =', int(3.89))
+
+print('int(False) =', int(False))
+print('int(True) =', int(True))
+
+print('int("135") =', int("135"))
+print('int("-28") =', int("-28"))
+print('int("  70") =', int("  70"))
+
+# приведёт к ValueError
+# print('int("7.6") =', int("7.6"))
+# print('int("91 01") =', int("91 01"))
+# print('int("1a1") =', int("1a1"))
