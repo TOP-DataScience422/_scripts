b4ccdf924822d432fb355e3348f8732abeb35e2a
Gennadiy <renardes@gmail.com>
2024-04-08 19:20:06 +0500

    преобразование объектов разных типов в тип float

---
+++ base/types2.py
@@ -0,0 +1,18 @@
+print('float(2001) =', float(2001))
+
+print('float(3.89) =', float(3.89))
+
+print('float(False) =', float(False))
+print('float(True) =', float(True))
+
+print('float("135") =', float("135"))
+print('float("-28") =', float("-28"))
+print('float("  70") =', float("  70"))
+print('float("7.6") =', float("7.6"))
+print('float("-3.4") =', float("-3.4"))
+
+# приведёт к ValueError
+# print('float("7,6") =', float("7,6"))
+# print('float("1.1.1") =', float("1.1.1"))
+# print('float("91 0.1") =', float("91 0.1"))
+# print('float("1a1") =', float("1a1"))
