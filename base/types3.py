1dbb5b6f258537ec397e241b93bdf1c835b1da3d
Gennadiy <renardes@gmail.com>
2024-04-13 22:28:02 +0500

    явная проверка на истинность

---
+++ base/types3.py
@@ -0,0 +1,16 @@
+print('bool(0) =', bool(0))
+print('bool(11) =', bool(11))
+print('bool(-2) =', bool(-2), end='\n\n')
+
+print('bool(0.0) =', bool(0.0))
+print('bool(0.0000000000001) =', bool(0.0000000000001))
+print('bool(-1.1) =', bool(-1.1), end='\n\n')
+
+print('bool("") =', bool(""))
+print('bool(" ") =', bool(" "))
+print('bool("python") =', bool("python"), end='\n\n')
+
+print('bool([]) =', bool([]))
+print('bool([""]) =', bool([""]))
+print('bool([1, 2]) =', bool([1, 2]), end='\n\n')
+
