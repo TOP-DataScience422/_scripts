4ef299ac7c75f1c05518627a13b73c99eb8973ce
Gennadiy <renardes@gmail.com>
2024-04-13 22:27:22 +0500

    операторы для работы с числами и последовательностями

---
+++ base/operators.py
@@ -0,0 +1,59 @@
+# операторы для работы с типом int
+# >>> 1 + 2
+# 3
+# >>> 5 - 7
+# -2
+# >>> 3 * 6
+# 18
+# >>> 10 / 6
+# 1.6666666666666667
+# >>> 20 / 2
+# 10.0
+# >>> 10 // 6
+# 1
+# >>> 13 // 12
+# 1
+# >>> 10 % 6
+# 4
+# >>> 8 % 2
+# 0
+# >>> 2 ** 8
+# 256
+
+
+# операторы для работы с типом float
+# >>> 1 + 2.0
+# 3.0
+# >>> 5 - 7.0
+# -2.0
+# >>> 10.0 // 6
+# 1.0
+# >>> 10 % 6.0
+# 4.0
+
+
+# операторы для работы с последовательностями
+# >>> 'abc' + 'def'
+# 'abcdef'
+# >>>
+# >>> [1, 2] + [-1, -2]
+# [1, 2, -1, -2]
+# >>>
+# >>> 'abc' * 3
+# 'abcabcabc'
+# >>>
+# >>> [0] * 8
+# [0, 0, 0, 0, 0, 0, 0, 0]
+# >>>
+# >>>
+# >>> 'abc' * 3.0
+# TypeError: can't multiply sequence by non-int of type 'float'
+# >>>
+# >>> 'abc' + [1, 2]
+# Traceback (most recent call last):
+  # File "<stdin>", line 1, in <module>
+# TypeError: can only concatenate str (not "list") to str
+# >>>
+# >>> range(5) + range(5, 10)
+# TypeError: unsupported operand type(s) for +: 'range' and 'range'
+
