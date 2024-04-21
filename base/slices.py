524ec14fb85adda4ec7915e64a09a64e7382c10b
Gennadiy <renardes@gmail.com>
2024-04-21 19:24:02 +0500

    способы взятия срезов

---
+++ base/slices.py
@@ -0,0 +1,108 @@
+# >>> text
+# 'python is the best'
+# >>>
+# >>> text[7:9]
+# 'is'
+# >>>
+# >>> text[10:12]
+# 'th'
+# >>> text[10:13]
+# 'the'
+# >>>
+# >>> words
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>> words[1:2]
+# ['of']
+# >>>
+# >>> words[1:3]
+# ['of', 'four']
+# >>>
+# >>>
+# >>> text
+# 'python is the best'
+# >>>
+# >>> text[0:6]
+# 'python'
+# >>>
+# >>> text[:6]
+# 'python'
+# >>>
+# >>> text[17]
+# 't'
+# >>> text[10:17]
+# 'the bes'
+# >>>
+# >>> text[10:18]
+# 'the best'
+# >>>
+# >>> text[10:100]
+# 'the best'
+# >>>
+# >>> text[100]
+# IndexError: string index out of range
+# >>>
+# >>> text[10:]
+# 'the best'
+# >>>
+# >>> text[-10:-2]
+# 's the be'
+# >>>
+# >>>
+# >>> text
+# 'python is the best'
+# >>>
+# >>> text[9:4]
+# ''
+# >>> words[3:2]
+# []
+# >>>
+# >>> words[:]
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>> words.copy()
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>>
+# >>>
+# >>> words
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>> words[::2]
+# ['phrase', 'four']
+# >>>
+# >>>
+# >>>
+# >>> text
+# 'python is the best'
+# >>>
+# >>> text[2:12:3]
+# 'tnsh'
+# >>>
+# >>> text[::-1]
+# 'tseb eht si nohtyp'
+# >>>
+# >>> words
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>> words[::-1]
+# ['words', 'four', 'of', 'phrase']
+# >>>
+# >>> words
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>> words.reverse()
+# >>> words
+# ['words', 'four', 'of', 'phrase']
+# >>>
+# >>> words.reverse()
+# >>> words
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>>
+# >>> words
+# ['phrase', 'of', 'four', 'words']
+# >>>
+# >>> words[1:-1]
+# ['of', 'four']
+
