cb04339899938346533cab89c61f6f95075fa998
Gennadiy <renardes@gmail.com>
2024-04-21 19:24:01 +0500

    вложенные циклы

---
+++ base/for4.py
@@ -0,0 +1,25 @@
+for i in range(5):
+    for j in range(4):
+        print(f'{i = }  {j = }')
+
+# i = 0  j = 0
+# i = 0  j = 1
+# i = 0  j = 2
+# i = 0  j = 3
+# i = 1  j = 0
+# i = 1  j = 1
+# i = 1  j = 2
+# i = 1  j = 3
+# i = 2  j = 0
+# i = 2  j = 1
+# i = 2  j = 2
+# i = 2  j = 3
+# i = 3  j = 0
+# i = 3  j = 1
+# i = 3  j = 2
+# i = 3  j = 3
+# i = 4  j = 0
+# i = 4  j = 1
+# i = 4  j = 2
+# i = 4  j = 3
+
