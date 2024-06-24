5de381b97eb3d7d0c666ad3b13fdd440f5e3c45e
Gennadiy <renardes@gmail.com>
2024-06-24 07:01:31 +0500

    линейно-взвешенная комбинация векторов

---
+++ math/linalg/vectors6.py
@@ -0,0 +1,15 @@
+from numpy import array
+
+
+v1 = array([4, 5, 1])
+v2 = array([-4, 0, -4])
+v3 = array([1, 3, 2])
+
+l1, l2, l3 = 1, 2, -3
+
+# >>> v1 + v2 + v3
+# array([ 1,  8, -1])
+
+# >>> l1*v1 + l2*v2 + l3*v3
+# array([ -7,  -4, -13])
+
