0c1114e5ba4ed62c4bf335252c00f5c4fe8f213f
Gennadiy <renardes@gmail.com>
2024-08-04 18:05:33 +0500

    дополнительные меры центральной тенденции

--- math/stat/means1.py
+++ math/stat/means1.py
@@ -1,5 +1,6 @@
-from numpy import array, mean, dot
+from numpy import array, mean, dot, average, median
 from numpy import set_printoptions
+from numpy.ma import sort
 
 
 set_printoptions(suppress=True)
@@ -47,4 +48,33 @@ p2 = array([.01, .01, .01, .01, .01, .01, .94])
 # >>>
 # >>> 10 * dot(x, p2)
 # np.float64(182.89999999999998)
+
+
+x3_sample = array([38, 39, 40, 38, 39, 41, 40, 42, 38, 45])
+
+x3 = array([38, 39, 40, 41, 42, 45])
+w3 = array([.3, .2, .2, .1, .1, .1])
+
+# среднее взвешенное
+# >>> average(x3, weights=w3)
+# np.float64(40.000000000000014)
+# >>>
+# >>> dot(x3, w3)
+# np.float64(40.00000000000001)
+
+
+# медиана
+# >>> sort(x3_sample)
+# array([38, 38, 38, 39, 39, 40, 40, 41, 42, 45])
+# >>> 
+# >>> h = len(x3_sample)
+# >>> 
+# >>> (sort(x3_sample)[h//2-1] + sort(x3_sample)[h//2]) / 2
+# np.float64(39.5)
+# >>> 
+# >>> median(x3_sample)
+# np.float64(39.5)
+
+# >>> median(x3)
+# np.float64(40.5)
 
