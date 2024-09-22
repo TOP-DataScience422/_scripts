290cc8ce609bc7fc4e1b0bea848ee1a06b074733
Gennadiy <renardes@gmail.com>
2024-09-22 16:20:07 +0500

    тестовые данные для кластеризации

---
+++ ml/clusters_generate.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+from numpy import array, savetxt
+
+from pathlib import Path
+from random import randrange as rr, uniform, shuffle
+from sys import path
+
+
+output_path = Path(path[0]) / 'test_clusters.csv'
+
+
+cluster1, cluster2, cluster3 = [], [], []
+
+for _ in range(rr(20, 30)):
+    cluster1.append((uniform(-2.5, -0.5), uniform(1.5, 4)))
+
+for _ in range(rr(20, 30)):
+    cluster2.append((uniform(-1, 2.5), uniform(3, 5.5)))
+
+for _ in range(rr(20, 30)):
+    cluster3.append((uniform(1, 3.5), uniform(0, 2)))
+
+
+data = cluster1 + cluster2 + cluster3
+shuffle(data)
+data = array(data)
+
+savetxt(output_path, data, fmt='%.1f', delimiter=',')
+
+plt.scatter(data.T[0], data.T[1])
+plt.show()
