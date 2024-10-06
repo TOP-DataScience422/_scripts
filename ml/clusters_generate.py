9f07f76471e6a169862896aa7163aad48f6b4ae8
Gennadiy <renardes@gmail.com>
2024-10-06 21:33:36 +0500

    тестовые данные для невыпуклых кластеров

--- ml/clusters_generate.py
+++ ml/clusters_generate.py
@@ -6,19 +6,19 @@ from random import randrange as rr, uniform, shuffle
 from sys import path
 
 
-output_path = Path(path[0]) / 'test_clusters.csv'
+output_path = Path(path[0]) / 'test_clusters_mixed.csv'
 
 
 cluster1, cluster2, cluster3 = [], [], []
 
 for _ in range(rr(20, 30)):
-    cluster1.append((uniform(-2.5, -0.5), uniform(1.5, 4)))
+    cluster1.append((uniform(-2, 0), uniform(1.5, 4)))
 
 for _ in range(rr(20, 30)):
-    cluster2.append((uniform(-1, 2.5), uniform(3, 5.5)))
+    cluster2.append((uniform(-1.5, 1.5), uniform(3, 5.5)))
 
 for _ in range(rr(20, 30)):
-    cluster3.append((uniform(1, 3.5), uniform(0, 2)))
+    cluster3.append((uniform(0, 2.5), uniform(1, 3)))
 
 
 data = cluster1 + cluster2 + cluster3
